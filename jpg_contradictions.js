ca8b5a94f4f760fe75c3e530f2e9d3fa0cd4c384
alexlafetra <alexlafetrathompson@gmail.com>
2025-10-21 01:31:53 -0700

    Added mobile styling

--- jpg_contradictions.js
+++ jpg_contradictions.js
@@ -1,5 +1,6 @@
 const imageAddress = "data/clouds.jpeg";
-const imageSizeLimit = 300000; //300kB file size limit
+const imageSizeLimit = 350000; //300kB file size limit
+let headerSize = 0;
 let MIMEType = "";
 let img;
 let textEntryCursor = {
@@ -10,11 +11,57 @@ let textEntryCursor = {
 
 let binaryDataString = "";
 
+//from: https://viereck.ch/jpeg-header/jpeg-header.js
+// Author: Thomas Lochmatter, thomas.lochmatter@viereck.ch
+// License: MIT
+
+// Returns an object with the width and height of the JPEG image stored in bytes, or null if the bytes do not represent a JPEG image.
+function readJpegHeader(bytes) {
+	// JPEG magick
+	if (bytes[0] != 0xff) return;
+	if (bytes[1] != 0xd8) return;
+
+	// Go through all markers
+	let pos = 2;
+	const dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+	while (pos + 4 < bytes.byteLength) {
+		// Scan for the next start marker (if the image is corrupt, this marker may not be where it is expected)
+		if (bytes[pos] != 0xff) {
+			pos += 1;
+			continue;
+		}
+
+		const type = bytes[pos + 1];
+
+		// Short marker
+		pos += 2;
+		if (bytes[pos] == 0xff) continue;
+
+		// SOFn marker
+		const length = dv.getUint16(pos);
+		if (pos + length > bytes.byteLength) return;
+		if (length >= 7 && (type == 0xc0 || type == 0xc2)) {
+			const data = {};
+			data.progressive = type == 0xc2;
+			data.bitDepth = bytes[pos + 2];
+			data.height = dv.getUint16(pos + 3);
+			data.width = dv.getUint16(pos + 5);
+			data.components = bytes[pos + 7];
+      data.headerSize = pos;
+      headerSize = pos;
+			return data;
+		}
+
+		// Other marker
+		pos += length;
+	}
+
+	return;
+}
 
 function setErrorMessage(message){
-  const errorMessageContainer = document.getElementById("error_container");
-  errorMessageContainer.style.display = "block";
-  errorMessageContainer.innerHTML = message;
+  document.getElementById("error_container").style.display =(window.innerWidth<1000)?"flex":"block";
+  document.getElementById("secret_text").innerHTML = message;
 }
 
 function clearError(){
@@ -25,11 +72,34 @@ function openFileSelector(){
   document.getElementById("file_selector").click();
 }
 
+function resizeImageAndCanvas(){
+    let sf_x = 1;
+    let sf_y = 1;
+    const isMobile = (window.innerWidth<1000);
+    const maxImageHeight = isMobile?(window.innerHeight - 450):(window.innerHeight - 250);
+    const maxImageWidth = isMobile?(window.innerWidth):(window.innerWidth-500)
+    if(img.height > maxImageHeight){
+      sf_y = maxImageHeight/img.height;
+    }
+    if(img.width > maxImageWidth){
+      sf_x = maxImageWidth/img.width;
+    }
+    const sf = Math.min(sf_x,sf_y);
+    resizeCanvas(img.width*sf,img.height*sf);
+    document.body.style.setProperty("--image-width", width+"px");
+    document.body.style.setProperty("--image-height", height+"px");
+    rerender();
+}
+
+function windowResized(){
+  resizeImageAndCanvas();
+}
+
 async function loadNewImage(event){
 
   const files = event.target.files;
   MIMEType = files[0].type;
-  document.getElementById("file_type_text").innerHTML = MIMEType;
+  document.getElementById("file_type_text").innerHTML = MIMEType.split('/')[1];
 
   //if there are files, read em!
   if (files && files.length) {
@@ -44,9 +114,9 @@ async function loadNewImage(event){
     reader_image.addEventListener("load",() => {
       //when the image is loaded, resize canvas and rerender
       //Also, get a new binary string!
-      img = loadImage(reader_image.result,()=>{
-        resizeCanvas(img.width,img.height);
-        rerender();
+      loadImage(reader_image.result,(newImg)=>{
+        img = newImg;
+        resizeImageAndCanvas(img);
       });
     });
     reader_image.readAsDataURL(files[0]);
@@ -70,8 +140,9 @@ function saveImage(){
 
 
 function rerender(){
-  image(img,-img.width/2,-img.height/2,img.width,img.height);
-  //green ellipse, just to see if it worked (debug)
+  background(255);
+  image(img,-width/2,-height/2,width,height);
+  //cursor rectangle
   fill(255,0,100,200);
   noStroke();
   rect(textEntryCursor.x-width/2,textEntryCursor.y-height/2,8,16)
@@ -90,45 +161,53 @@ function getStringIndexFromPixelCoords(x,y){
     const blockY = Math.floor(y / 8);
     const blockIndex = blockY * blockWidth + blockX;
 
-    const bytesPerBlock = binaryDataString.length/totalBlocks;
+    //assuming all the bytes are pixel block data (they're not but it's okay)
+    const bytesPerBlock = (binaryDataString.length - headerSize)/totalBlocks;
     const approxByteIndex = Math.floor(blockIndex*bytesPerBlock);
 
     return approxByteIndex;
   }
+  else if(MIMEType == 'image/png'){
+    //png header is 29 bytes
+    const bytesPerPixel = (binaryDataString.length - 29)/(img.width*img.height);
+    const pixelIndex = y * width + x;
+    const approxByteIndex = Math.trunc(bytesPerPixel*pixelIndex);
+    return approxByteIndex;
+  }
 }
 
-function setSecretText(){
+function setInputText(){
+  //set the red, secret text & scroll to it
   const secretTextElement = document.getElementById("secret_text");
   secretTextElement.innerHTML = document.getElementById("secret_input").value;
-  secretTextElement.scrollIntoView({
-    behavior:'instant',//jump instantly
-    block: 'center'//center it vertically
-  });
+  //scroll the div to show the text that was just set
+  secretTextElement.parentNode.scrollTop = secretTextElement.offsetTop - secretTextElement.parentNode.offsetTop;
+
 }
 
 function setDataText(){
   //set the two chunks of text that won't change
   document.getElementById("text_start").innerHTML = binaryDataString.slice(0,textEntryCursor.index);
   document.getElementById("text_end").innerHTML = binaryDataString.slice(textEntryCursor.index);
+  setInputText();
 }
 
 //called by the onclick event of the "enter" button
-function enterText(){
+function submitText(){
   const htmlTextInputElement = document.getElementById("secret_input");
   recompileImage(binaryDataString.slice(0,textEntryCursor.index)+htmlTextInputElement.value+binaryDataString.slice(textEntryCursor.index));
-  setSecretText();
+  setInputText();
 }
 
 function mouseClicked(){
   if(mouseX<width && mouseY < height && mouseX > 0 && mouseY >0){
-    console.log("x:",mouseX);
-    console.log("y:",mouseY);
     textEntryCursor = {
-      x:mouseX,
-      y:mouseY,
+      x:Math.trunc(mouseX),
+      y:Math.trunc(mouseY),
       index : getStringIndexFromPixelCoords(mouseX,mouseY)
     };
-    enterText();
+    document.getElementById("coordinate_text").innerHTML = '('+textEntryCursor.x+","+textEntryCursor.y+')';
+    submitText();
     setDataText();
   }
 }
@@ -160,8 +239,10 @@ function recompileImage(dataString){
 }
 
 function bufferToBinaryString(buffer){
+
   let binaryString = '';
   const bytes = new Uint8Array(buffer);
+  readJpegHeader(bytes);
   const len = bytes.byteLength;
   for(let i = 0; i<len; i++){
     binaryString += String.fromCharCode(bytes[i]);
@@ -183,6 +264,7 @@ function setup(){
       setDataText();
     });
   createCanvas(img.width,img.height,WEBGL);
-  image(img,-img.width/2,-img.height/2,img.width,img.height);
+  resizeImageAndCanvas(img);
+  rerender();
   noLoop();
 }
